70678c8a22e5118c114bb55e6358902ac6083f49
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-04-24 18:57:01 -0400

    Update model training

--- ML/utils.py
+++ ML/utils.py
@@ -5,28 +5,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
-def load_data():
+def load_data(file_paths):
 
-    # Load JSON data from the file
-    with open("data.json", "r") as file:
-        json_data = json.load(file)
+    all_x_data = []
+    all_y_data = []
+    
+    for file_path in file_paths:
+        with open(file_path, "r") as file:
+            json_data = json.load(file)
+        
+        x_data = [sample['xs'] for sample in json_data['data']]
+        x_data = [[coord for dict_ in entry for coord in (dict_['x'], dict_['y'], dict_['z'])] for entry in x_data]
+        all_x_data.extend(x_data)  
 
-    x_data = [sample['xs'] for sample in json_data['data']]
-    x_data = [[coord for dict_ in entry for coord in (dict_['x'], dict_['y'], dict_['z'])] for entry in x_data]
+        y_data = []
+        for sample in json_data['data']:
+            label = sample['ys']['0']
+            if label == 'o':
+                y_data.append(1)
+            elif label == 'n':
+                y_data.append(0)
+        all_y_data.extend(y_data)  
 
-    x_data = np.array(x_data)
+    all_x_data = np.array(all_x_data)
+    all_y_data = np.array(all_y_data)
 
-    y_data = []
-    for sample in json_data['data']:
-        label = sample['ys']['0']
-        if label == 'o':
-            y_data.append(1)
-        elif label == 'n':
-            y_data.append(0) 
-
-    y_data = np.array(y_data)
-
-    return x_data, y_data
+    return all_x_data, all_y_data
 
 
 def shuffle_data(x_data, y_data, random_seed=2):
@@ -41,6 +45,7 @@ def shuffle_data(x_data, y_data, random_seed=2):
 
 
 def split_data(x_data, y_data, split_idx):
+
     x_train, y_train = x_data[:split_idx], y_data[:split_idx]
     x_test, y_test = x_data[split_idx:], y_data[split_idx:]
 
@@ -48,6 +53,9 @@ def split_data(x_data, y_data, split_idx):
 
 
 def preprocess_data(x_train, y_train, x_test, y_test):
+
+    x_train = x_train.reshape(-1, 63)
+    x_test = x_test.reshape(-1, 63)
 
     val_size = int(len(x_train) * 0.1)
 
@@ -57,15 +65,31 @@ def preprocess_data(x_train, y_train, x_test, y_test):
     return x_train, y_train, x_val, y_val, x_test, y_test
 
 
+def build_baseline(input_shape=63):
+
+    model = keras.models.Sequential([
+
+        # Dense layer with 64 units
+        layers.Dense(64, input_shape = (input_shape,), activation='relu'),  
+        layers.Dense(32, activation='relu'),  
+        layers.Dense(2, activation="relu"),  
+
+        # Sigmoid output layer 
+        layers.Dense(1, activation='sigmoid')  
+    ])
+
+    model.compile(optimizer="adam",
+              loss="binary_crossentropy",
+              metrics=["accuracy"])
+    
+    return model
+
 def build_model(input_shape=63):
 
     model = keras.models.Sequential([
-        # The dense neural network model consists of a flatten layer
 
-        # Dense layer with 256 units
-        layers.Dense(256, input_shape = (input_shape,), activation='relu'),  
-
-        # Dropout layer with 0.5 dropout rate
+        # Dense layer with 64 units
+        layers.Dense(64, input_shape = (input_shape,), activation='relu'),  
         layers.Dense(10, activation="relu"),  
 
         # Sigmoid output layer 
@@ -130,32 +154,32 @@ def plot_loss(history):
 
 def explore_data(X_train, y_train, y_test, y_val):
 
-  # Class names
-  class_names = ['o', 'n']
+    # Class names
+    class_names = [0, 1]
 
-  # Plot the distribution of classes in the training, validation, and test sets
-  fig, ax = plt.subplots(1, 3, figsize=(10, 5))
+    # Plot the distribution of classes in the training, validation, and test sets
+    fig, ax = plt.subplots(1, 3, figsize=(10, 5))
 
-  # Plot the distribution of classes in the training set
-  train_class_counts = np.bincount(y_train)
-  ax[0].bar(range(len(class_names)), train_class_counts)
-  ax[0].set_xticks(range(len(class_names)))
-  ax[0].set_xticklabels(class_names, rotation=45)
-  ax[0].set_title('Training set')
+    # Plot the distribution of classes in the training set
+    train_class_counts = np.bincount(y_train)
+    ax[0].bar(range(len(class_names)), train_class_counts)
+    ax[0].set_xticks(range(len(class_names)))
+    ax[0].set_xticklabels(class_names, rotation=45)
+    ax[0].set_title('Training set')
 
-  # Plot the distribution of classes in the test set
-  test_class_counts = np.bincount(y_val)
-  ax[1].bar(range(len(class_names)), test_class_counts)
-  ax[1].set_xticks(range(len(class_names)))
-  ax[1].set_xticklabels(class_names, rotation=45)
-  ax[1].set_title('Val set')
+    # Plot the distribution of classes in the test set
+    test_class_counts = np.bincount(y_val)
+    ax[1].bar(range(len(class_names)), test_class_counts)
+    ax[1].set_xticks(range(len(class_names)))
+    ax[1].set_xticklabels(class_names, rotation=45)
+    ax[1].set_title('Val set')
 
-  # Plot the distribution of classes in the test set
-  test_class_counts = np.bincount(y_test)
-  ax[2].bar(range(len(class_names)), test_class_counts)
-  ax[2].set_xticks(range(len(class_names)))
-  ax[2].set_xticklabels(class_names, rotation=45)
-  ax[2].set_title('Test set')
+    # Plot the distribution of classes in the test set
+    test_class_counts = np.bincount(y_test)
+    ax[2].bar(range(len(class_names)), test_class_counts)
+    ax[2].set_xticks(range(len(class_names)))
+    ax[2].set_xticklabels(class_names, rotation=45)
+    ax[2].set_title('Test set')
 
-  plt.show()
+    plt.show()
 
